bcf94c115b98c3ef1489d92b004b3fd5e2614205
Adillson Gonçalves Valeriano <34108107+Adilsongv@users.noreply.github.com>
2024-09-09 18:35:05 -0300

    Projeto Ok

---
+++ app.js
@@ -0,0 +1,46 @@
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("buscaLivro").value;
+
+    console.log(campoPesquisa);
+
+    if(campoPesquisa == ""){
+        section.innerHTML = `<p> Pesquisa vazia não retorna valor, por favor digite algo. </p>`
+        return
+    }
+campoPesquisa = campoPesquisa.toLowerCase();
+
+let resultados = "";
+let titulo = "";
+let escritor = "";
+let resenha = "";
+
+for (let dado of dados){
+    titulo = dado.Titulo.toLowerCase();
+    escritor = dado.Escritor.toLowerCase();
+    resenha = dado.resenha.toLowerCase();
+
+    if(titulo.includes(campoPesquisa) || escritor.includes(campoPesquisa) || resenha.includes(campoPesquisa) ){
+    
+        resultados += `
+        <div class="item-resultado">
+            <div class="Img">
+                <img src=${dado.Imagem} alt= ${dado.Titulo}  width="100px">
+            </div>
+            <div class="resenha">
+                <h2>${dado.Titulo} -  ${dado.Escritor}</h2>
+                <p class="descricao-meta">${dado.resenha}</p>
+            </div>
+        </div>
+    `;
+    }
+
+    if(!resultados){
+        resultados = `<p> Nada foi encontrado. </p>`
+    }
+}
+
+section.innerHTML = resultados;
+}
+
---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    {
+        Imagem: "img/noitemaldita.jpg" ,
+        Escritor: "Andre Vianco",
+        link_escritor:"https://pt.wikipedia.org/wiki/Andr%C3%A9_Vianco",
+        Titulo:"Noite Maldita",
+        resenha:"Uma noite que parecia ser apenas mais uma noite virou o inicio de uma saga épica, onde conheceremos seres sobrenaturais da noite e uns poucos humanos que ja nascem sem medo de lutar."
+    },
+    {
+        Imagem: "img/aderiva.jpg" ,
+        Escritor: "Andre Vianco",
+        link_escritor:"https://pt.wikipedia.org/wiki/Andr%C3%A9_Vianco",
+        Titulo:"A Deriva",
+        resenha:"Após fugirem de uma São Paulo Agora infestada de Vampiros os sobreviventes tem a dificil missão de recomeçar e reorganizar-se nesse novo mundo ainda sob a ameaça de ataques tanto de vampiros como de humanos com ideais mesquinhos."
+    },
+    {
+        Imagem: "img/bento1.jpg" ,
+        Escritor: "Andre Vianco",
+        link_escritor:"https://pt.wikipedia.org/wiki/Andr%C3%A9_Vianco",
+        Titulo:"Bento",
+        resenha:"Heroi humano e inimigo vampiro, a saga dos bentos tem o ponto culminante 30 anos após a Noite maldita, as fortificações por todo o Brasil se encontram longe das grandes cidades , mas o medo continua."
+    },
+    {
+        Imagem: "img/bento2.jpg" ,
+        Escritor: "Andre Vianco",
+        link_escritor:"https://pt.wikipedia.org/wiki/Andr%C3%A9_Vianco",
+        Titulo:"Vampiro Rei - Bruxa Tereza",
+        resenha:"Após os 4 milagres parece que aos poucos os humanos irão retormar o mundo .. só que não, ainda mais se depender da Bruxa Tereza."
+    },
+    {
+        Imagem: "img/bento3.jpg" ,
+        Escritor: "Andre Vianco",
+        link_escritor:"https://pt.wikipedia.org/wiki/Andr%C3%A9_Vianco",
+        Titulo:"Vampiro Rei - Cantarzo",
+        resenha:"A hora dos Vampiros chegou, com a ajuda da Bruxa e o sangue de 30 bentos serão criadas criaturas miticas que irão abalar a terra novamente .. qual será o destino da humanidade?"
+    }
+]
